97a2bf963632e8a75a7a1f97e7029d79c495b386
el-joe <38781177+el-joe@users.noreply.github.com>
2021-09-23 01:25:02 +0200

    Update PusherServiceProvider.php

--- src/PusherServiceProvider.php
+++ src/PusherServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace myPusher\Pusher;
+namespace PusherJ;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,7 +13,7 @@ class PusherServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->make('myPusher\Pusher\PusherController'); // <- when create new controller
+        $this->app->make('PusherJ\PusherController'); // <- when create new controller
     }
 
     /**
